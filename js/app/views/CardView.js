aeca325b1595d6b19f2cbe128ead1d7e7a0bd682
alairfonseca <alairfonseca13@gmail.com>
2017-05-18 00:03:28 -0300

    .

--- js/app/views/CardView.js
+++ js/app/views/CardView.js
@@ -6,12 +6,14 @@ class CardView extends View {
 
   template() {
     return `
-            <div class="card card-outline-primary mb-3 text-center">
-              <div class="card-block">
-              <blockquote class="card-blockquote">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-              <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-              </blockquote>
+            <div class="row" draggable="true" ondragstart="drag(event)">
+              <div class="card card-outline-primary mb-3 text-center">
+                <div class="card-block">
+                <blockquote class="card-blockquote">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
+                </div>
               </div>
             </div>
            `;
